d91cd1778c0df930ed8129cfc17250c381ac4b9e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-04-15 13:52:42 -0500

    Stackblitz reproduction (#1)
    * feat(express): install express
    
    * feat: remove /hacker.php endpoint from demo app
    
    * feat(exfil): serve exfil app separately from demo app
    
    * feat(): Prepare for stackblitz reproduction

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the `public` directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/hacker.php', (req, res) => {
+  console.log('XSS Attack successful', req)
+  return res.send(200, { message: req.query });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
